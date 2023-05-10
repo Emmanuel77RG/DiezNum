2ec7dd73883badeb63d6eb485e0c3878c0d8551c
Emmanuel77RG <124705771+Emmanuel77RG@users.noreply.github.com>
2023-05-09 19:59:49 -0600

    Add files via upload

---
+++ Dieznum.java
@@ -0,0 +1,19 @@
+
+public class Dieznum
+{
+    public void ejercicio1()
+    {
+
+        int i = 1;
+    do {
+        System.out.println(i);
+        i++;
+        
+        } while (i <= 10);
+    }
+
+    public static void main(String[] args) {
+        Dieznum rune = new Dieznum();
+        rune.ejercicio1();
+    }
+}
